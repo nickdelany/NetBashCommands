c786df48d8e1bfb4653cfe1eded587b0edc7604c
Nick Delany <nick.delany@gmail.com>
2013-02-21 22:55:10 +0000

    Initial Commit

---
+++ NetBashCommands/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetCommands")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetCommands")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b47ca47-9104-45e8-b496-94ab6bea6fb3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetBashCommands/Reflection/AssembliesCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NetBash;
+
+namespace NetBashCommands.Reflection
+{
+    [WebCommand("assemblies", "Display loaded assemblies. Use ? for search options.")]
+    public class AssembliesCommand : IWebCommand
+    {
+        private const string Help = 
+@"nnn - assembly name starts with nnn
++nnn - assembly name contains nnn
+!nnn - assembly name does not start with nnn
+-nnn - assembly name does not contain nnn
+a +b -c !d returns
+names beginning with a and containing b 
+that do not contain c nor start with d";
+
+        public bool ReturnHtml
+        {
+            get { return false; }
+        }
+
+        public string Process(string[] args)
+        {
+            if (args.Length == 1 && args[0].Equals("?"))
+                return Help;
+
+            StringBuilder ret = new StringBuilder();
+
+            var begins = args.Where(a => !(a.StartsWith("-") || a.StartsWith("+") || a.StartsWith("!"))).ToArray();
+            var contains = args.Where(a => a.StartsWith("+")).ToArray();
+            var notBegins = args.Where(a => a.StartsWith("!")).ToArray();
+            var notContains = args.Where(a => a.StartsWith("-")).ToArray();
+
+            foreach (AssemblyName assembly in 
+                AppDomain.CurrentDomain.GetAssemblies()
+                    .Select(assembly => assembly.GetName())
+                    .Where(assembly => args.Length == 0 
+                        || MatchesSearchTerm(begins, contains, notBegins, notContains, assembly.Name)))
+            {
+                ret.AppendLine(string.Format("{0}: {1}", assembly.Name, assembly.Version));
+            }
+
+            return ret.ToString();
+        }
+
+        private static bool MatchesSearchTerm(string[] begins, string[] contains, string[] notBegins, string[] notContains, string name)
+        {
+            string lower = name.ToLowerInvariant();
+
+            return
+                (begins.Length == 0 || begins.Any(a => lower.StartsWith(a.ToLowerInvariant())))
+             && (contains.Length == 0 || contains.Any(a => lower.Contains(a.Substring(1).ToLowerInvariant())))
+             && (notContains.Length == 0 || !notContains.Any(a => lower.Contains(a.Substring(1).ToLowerInvariant())))
+             && (notBegins.Length == 0 || !notBegins.Any(a => lower.StartsWith(a.Substring(1).ToLowerInvariant())));
+        }
+    }
+}
+ 
